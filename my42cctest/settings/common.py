38253aef990887de441d3188f80bc31ebf051d5a
khchine5 <hamzakhchine@gmail.com>
2018-10-02 22:29:58 +0100

    42-test-khachine5#1 : Change database name (Force recreate the database).

--- my42cctest/settings/common.py
+++ my42cctest/settings/common.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'my42cctest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_my42cctest.sqlite3'),
     }
 }
 
